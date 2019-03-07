553694048374d5ef68c18739d84197fcb68f68b0
Elyin-Anuar <47902996+Elyin-Anuar@users.noreply.github.com>
2019-03-06 18:43:05 -0600

    V

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using Sales.Common.Models;
+using Sales.ViewModels.Sales.ViewModels;
+
+namespace Sales.ViewModels
+{
+
+ 
+    public class ProductsViewModel : BaseViewModel
+    {
+        private ObservableCollection<Product> products { get; set; }
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+    }
+}
